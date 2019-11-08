ba216c17627e1aae2cd81f835a31555aeb6a37db
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-08 03:12:16 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,7 @@ class StudentsController < ApplicationController
   
   def create 
     @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    binding.pry 
     redirect_to new_student_path
   end 
 
